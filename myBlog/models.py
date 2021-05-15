db4f377fca4c13c4619b399682ba100dee7ff25f
maliibrahim <mohamed.su37@gmail.com>
2021-05-15 22:28:21 +0400

    deploying to heroku

--- myBlog/models.py
+++ myBlog/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from datetime import datetime, date
 from ckeditor.fields import RichTextField
 
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    pid = models.CharField(null=True, blank=True, max_length=255)
 
     def __str__(self):
         return self.name
